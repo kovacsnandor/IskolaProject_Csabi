9ae06951aa4cfcee741bda983a2e584a98f3f74b
csabinyoo <mdgycsba@gmail.com>
2025-01-24 08:11:23 +0100

    *

--- Frontend/src/stores/useAuthStore.js
+++ Frontend/src/stores/useAuthStore.js
@@ -2,28 +2,28 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    id: Number(localStorage.getItem('id')) || null,
-    user: localStorage.getItem('user') || null,
-    token: localStorage.getItem('currentToken') || null,
+    id: Number(sessionStorage.getItem('id')) || null,
+    user: sessionStorage.getItem('user') || null,
+    token: sessionStorage.getItem('currentToken') || null,
   }),
   getters: {},
   actions: {
     setId(id) {
-      localStorage.setItem('id', id);
+      sessionStorage.setItem('id', id);
       this.id = id;
     },
     setUser(user) {
-      localStorage.setItem('user', user);
+      sessionStorage.setItem('user', user);
       this.user = user;
     },
     setToken(token) {
-      localStorage.setItem('currentToken', token);
+      sessionStorage.setItem('currentToken', token);
       this.token = token;
     },
     clearStoredData() {
-      localStorage.removeItem('id');
-      localStorage.removeItem('user');
-      localStorage.removeItem('currentToken');
+      sessionStorage.removeItem('id');
+      sessionStorage.removeItem('user');
+      sessionStorage.removeItem('currentToken');
       this.id = null;
       this.token = null;
       this.user = null;
